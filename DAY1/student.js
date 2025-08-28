2e444449b1f4aeb30a8752d18a5600bb2158c6e1
Alam Mewada <mewadaalam40@gmail.com>
2025-08-28 10:29:20 +0530

    day1

---
+++ DAY1/student.js
@@ -0,0 +1,14 @@
+// Create an object
+let student = {
+    name: "Alam Mewada",
+    age: 21,
+    course: "Computer Science",
+    rollNo: 102
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Roll No:", student.rollNo);
