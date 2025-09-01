9932dccc911d9a08f4838895cbcd7264217e9363
Vinodpra <avinashsinghprajapati07@gmail.com>
2025-09-01 14:05:13 +0530

    Create Day4.js
    API returning "Hello, World!"

---
+++ Day4.js
@@ -0,0 +1,15 @@
+// inatall lib, npm init -y, npm install express
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Define a simple API route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
